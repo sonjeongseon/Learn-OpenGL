80d8cb2f99572a0b09991ec46275f3b2b555742c
wjdtjs2430@gmail.com <wjdtjs2430@gmail.com>
2022-09-15 20:17:40 +0900

    chap2 공부한 부분 추가

---
+++ study/transform.fs
@@ -0,0 +1,15 @@
+ines (11 sloc)  311 Bytes
+
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoord;
+
+// texture samplers
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+}
